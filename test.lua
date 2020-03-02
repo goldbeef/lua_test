1254e7315d20d58ab4aab4ad68743eb1a10f9b52
Heng Zhao <heng.zhao@shopee.com>
2020-03-02 21:09:22 +0800

    up

--- test.lua
+++ test.lua
@@ -990,9 +990,77 @@ print((1 ~mask) < (0xffffffffffffffff ~mask)) --true
     文本 -> 二进制
 --]]
 
+--[[
 buff = string.pack("ii", 1,2)
 print(#buff)
 print(string.unpack("ii", buff))
+--]]
+
+--[[
+array
+	idx must start from 1;
+	for sequence opeartion
+--]]
+a = { }
+
+for i = 1,5 do
+    a[i] = i
+end
+print(#a)
+
+for i = -5,5 do
+    a[i] = i
+end
+print(#a)
+
+--[[
+matrix:
+	不规则数组(二维)
+	一层数组
+稀疏矩阵:
+	lua中不需要考虑，非nil元素才占用空间
+
+	矩阵乘法
+		调整遍历层次，优化
+--]]
+
+--[[
+链表
+	list = nil
+	list = {
+		next = list,
+		val = v,
+	}
+
+	local l = list
+	while l do
+		visit l.value
+		l = l.next
+	end
+双向列表
+循环列表
+	类似上面
+栈
+	无界数组即可
+--]]
+
+--[[
+--
+队列/双端队列
+	function listNew(){
+		return {
+			first = 0,
+			last = -1;
+		}
+	}
+
+	pushFirst(list, value);
+	pushLast(list, value);
+
+	popFirst(list, value);
+	popLast(list, value);
+--]]
+
 
 --[[
 function max(num1, num2)
