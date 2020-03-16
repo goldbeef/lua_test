c0932a0ccaf591c6182547e63e41773137f3bf41
Heng Zhao <heng.zhao@shopee.com>
2020-03-16 11:08:55 +0800

    up

--- test.lua
+++ test.lua
@@ -1337,6 +1337,7 @@ for k, v in pairs(_G)  do
 end
 --]]
 
+--[[
 function test1()
     print("test1")
 end
@@ -1355,6 +1356,37 @@ debug.sethook(hookCall,"r")
 print("hello1")
 test1()
 print("hello2")
+--]]
+
+--[[
+--json
+local cjson2 = require "cjson"
+-- 布尔类型
+local lua_bool = true
+print(cjson2.encode(lua_bool))
+
+-- 数组类型
+local lua_array = {1, 2, 3, 4, 5, 6}
+print(cjson2.encode(lua_array))
+
+-- 数值类型
+local lua_number = 6.66
+print(cjson2.encode(lua_number))
+
+-- 字符串类型
+local lua_string = "I am test1280"
+print(cjson2.encode(lua_string))
+
+-- 对象类型
+local lua_object = {
+    ["name"] = "Jiang",
+    ["age"] = 24,
+    ["addr"] = "BeiJing",
+    ["email"] = "1569989xxxx@126.com",
+    ["tel"] = {f='123',d='456'}
+}
+print(cjson2.encode(lua_object))
+--]]
 
 --[[
 function max(num1, num2)
